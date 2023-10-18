88ce4f30dbcacceececf08d74a258a4cc25ba24c
Chris Marker <cm@insomniacdesign.com>
2023-10-18 17:59:13 -0400

    Initial Commit

---
+++ munkyApe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  munkyApe
+//
+//  Created by Chris Marker on 10/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
