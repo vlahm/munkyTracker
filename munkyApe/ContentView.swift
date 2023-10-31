f55c3251f7a0be67668eb65722b692590d9908e5
Chris Marker <cm@insomniacdesign.com>
2023-10-31 18:13:38 -0400

    first commit

--- munkyApe/ContentView.swift
+++ munkyApe/ContentView.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
+                .imageScale(.small)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, munky!")
+            AsyncImage(url: URL(string: "https://images.unsplash.com/photo-1544377747-75ce8da6c699?auto=format&fit=crop&q=80&w=2764&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"))
+                .imageScale(.small)
         }
         .padding()
     }
